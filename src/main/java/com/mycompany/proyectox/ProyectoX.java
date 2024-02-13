377ef829fca7f50f7ed5537fd16bac203b32df0f
user <user@148.210.170.211>
2024-02-12 19:18:03 -0700

    Estoy haciendo el primer commit para ver como se hace

---
+++ src/main/java/com/mycompany/proyectox/ProyectoX.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectox;
+
+/**
+ *
+ * @author user
+ */
+public class ProyectoX {
+
+    public static void main(String[] args) {
+        System.out.println("Prueba 2!");
+    }
+}
