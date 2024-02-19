8e465ded772235d3fa21f120e506076e4b8088cd
user <user@148.210.170.211>
2024-02-18 19:59:26 -0700

    Sigo experimentando con esto, sorry XD

--- src/main/java/com/mycompany/proyectox/ProyectoX.java
+++ src/main/java/com/mycompany/proyectox/ProyectoX.java
@@ -11,6 +11,6 @@ package com.mycompany.proyectox;
 public class ProyectoX {
 
     public static void main(String[] args) {
-        System.out.println("Prueba 2!");
+        System.out.println("Prueba 3!");
     }
 }
